32e37ba58620b0beb59b34c2ebb37e50436ae5a7
DBattou <doucerain.b@gmail.com>
2025-02-06 12:13:13 +0100

    add a todo item

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import './App.css'
+import TodoItem from './components/TodoItem'
 
 function App() {
-  return <h1 className="text-orange-600">MY BIG TITLE</h1>
+  return (
+    <>
+      <h1 className="text-orange-600">MY BIG TITLE</h1>
+      <TodoItem
+        text="Learn React"
+        id={1}
+        completed={false}
+        onToggle={() => {}}
+        onDelete={() => {}}
+      />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const TodoItem: React.FC<{
+  text: string
+  id: number
+  completed: boolean
+  onToggle: (id: number) => void
+  onDelete: (id: number) => void
+}> = ({ text, id, completed, onToggle, onDelete }) => {
+  return (
+    <div className="flex items-center justify-between p-4 border-b">
+      <div className="flex items-center space-x-2">
+        <input
+          type="checkbox"
+          onClick={() => onToggle(id)}
+          checked={completed}
+          readOnly
+        />
+        <span
+          className={`text-lg ${
+            completed ? 'line-through text-gray-500' : 'text-grey-9000'
+          }`}
+        >
+          {text}
+        </span>
+      </div>
+      <button onClick={() => onDelete(id)} className="text-red-500 hover:text-red-700">
+        Supprimer
+      </button>
+    </div>
+  )
+}
+
+export default TodoItem
