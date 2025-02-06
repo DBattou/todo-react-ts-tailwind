db88702f5894f883bec648171867e37357191d43
Doucerain Baptiste <doucerain.b@gmail.com>
2025-02-06 14:32:25 +0100

    add todoList taking todos as parameters

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import './App.css'
-import TodoItem from './components/TodoItem'
+import { TodoItemProps } from './components/TodoItem'
+import TodoList from './components/TodoList'
+
+const todos: Array<TodoItemProps> = [
+  {
+    id: 1,
+    text: 'Learn React',
+    completed: false,
+    onToggle: () => {},
+    onDelete: () => {},
+  },
+  {
+    id: 2,
+    text: 'Learn TypeScript',
+    completed: false,
+    onToggle: () => {},
+    onDelete: () => {},
+  },
+]
 
 function App() {
   return (
     <>
       <h1 className="text-orange-600">MY BIG TITLE</h1>
-      <TodoItem
-        text="Learn React"
-        id={1}
-        completed={false}
-        onToggle={() => {}}
-        onDelete={() => {}}
-      />
+      <TodoList todos={todos} />
     </>
   )
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import TodoItem, { TodoItemProps } from './TodoItem'
+
+interface TodoListProps {
+  todos: Array<TodoItemProps>
+}
+
+const TodoList: React.FC<TodoListProps> = ({ todos: todosProp }) => {
+  const [todos, setTodos] = useState<TodoListProps['todos']>(todosProp)
+  const [newTodo, setNewTodo] = useState('')
+
+  const addTodo = () => {
+    if (newTodo.trim() === '') return
+
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      {
+        id: Math.random(),
+        text: newTodo,
+        completed: false,
+        onToggle: () => {},
+        onDelete: () => {},
+      },
+    ])
+    setNewTodo('')
+  }
+
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg">
+      <h1 className="text-2xl font-bold mb-4">Todo List</h1>
+      <div className="flex space-x-2 mb-4">
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          className="flex-1 p-2 border rounded"
+          placeholder="Ajouter une tâche"
+        />
+        <button
+          onClick={addTodo}
+          className={`px-4 py-2 bg-blue-500 text-white rounded ${
+            newTodo.trim() === ''
+              ? 'opacity-50 cursor-default hover:bg-blue-500'
+              : 'hover:bg-blue-600'
+          }`}
+        >
+          Ajouter
+        </button>
+      </div>
+
+      <div>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default TodoList
